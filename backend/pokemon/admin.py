59ba2ff2e21331bec563fa0ac253ac96478aac0d
Rumman Rahib <lu.rummanrahib@gmail.com>
2025-06-05 06:50:35 +0200

    feat: add model admin

--- backend/pokemon/admin.py
+++ backend/pokemon/admin.py
@@ -10,10 +10,35 @@ from .models import (
     Type,
 )
 
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pokedex_id')
+    search_fields = ('name',)
+    list_filter = ('types',)
+    ordering = ('pokedex_id',)
+    list_per_page = 100
+
+
+class PokemonMoveAdmin(admin.ModelAdmin):
+    list_display = ('pokemon', 'move', 'learn_method', 'level_learned')
+    search_fields = ('pokemon__name', 'move__name')
+    list_filter = ('learn_method',)
+    ordering = ('pokemon__name', 'move__name')
+    list_per_page = 100
+
+
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('name', 'move_type', 'damage_class')
+    search_fields = ('name',)
+    list_filter = ('move_type', 'damage_class')
+    ordering = ('name',)
+    list_per_page = 100
+
+
+admin.site.register(Pokemon, PokemonAdmin)
+admin.site.register(PokemonMove, PokemonMoveAdmin)
+admin.site.register(Move, MoveAdmin)
 admin.site.register(Type)
 admin.site.register(Ability)
 admin.site.register(EvolutionChain)
-admin.site.register(Move)
-admin.site.register(Pokemon)
-admin.site.register(PokemonMove)
 admin.site.register(PokemonSpecies)
